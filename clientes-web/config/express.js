855818ef3fa54d73f356f758f64a40f2b42b76b4
Wellington Rogério <wrogerio.ing@gmail.com>
2021-03-24 17:30:09 -0300

    .

--- clientes-web/config/express.js
+++ clientes-web/config/express.js
@@ -1,31 +1,32 @@
 // Requires
-const express = require("express");
-const config = require("config");
-const path = require("path");
-const router = require("../src/routes/routes");
+const express = require('express');
+const config = require('config');
+const path = require('path');
+const router = require('../src/routes/routes');
 
 module.exports = () => {
-    // setup App
-    const app = express();
-    app.set("port", process.env.PORT || config.get("server.port"));
+	// setup App
+	const app = express();
+	app.set('port', process.env.PORT || config.get('server.port'));
 
-    // middlewares
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
-    app.use(express.json({type: 'application/json'}));
+	// middlewares
+	app.use(express.urlencoded({ extended: true }));
+	app.use(express.json());
+	app.use(express.json({ type: 'application/json' }));
 
-    // Routes
-    app.use(router)
+	// Routes
+	app.use(router);
 
-    // static routes
-    app.use('/estilos', express.static(path.join(__dirname, '../src/assets/css')))
-    app.use('/scripts', express.static(path.join(__dirname, '../src/assets/js')))
-    app.use('/imagens', express.static(path.join(__dirname, '../src/assets/img')))
+	// static routes
+	app.use('/estilos', express.static(path.join(__dirname, '../src/assets/css')));
+	app.use('/scripts', express.static(path.join(__dirname, '../src/assets/js')));
+	app.use('/imagens', express.static(path.join(__dirname, '../src/assets/img')));
+    app.use('/lib', express.static(path.join(__dirname, '../src/assets/lib')));
 
-    // view engine
-    app.set('view engine', 'ejs')
-    app.set('views', path.join(__dirname, '../src/views'))
+	// view engine
+	app.set('view engine', 'ejs');
+	app.set('views', path.join(__dirname, '../src/views'));
 
-    // return
-    return app
+	// return
+	return app;
 };
--- clientes-web/src/assets/js/editar.js
+++ clientes-web/src/assets/js/editar.js
@@ -1,28 +1,33 @@
-const formUpdate = document.getElementById("formUpdate");
-const nome = document.getElementById("nome");
-const cpf = document.getElementById("cpf");
-const idade = document.getElementById("idade");
-const _id = document.getElementById("_id");
+const formUpdate = document.getElementById('formUpdate');
+const nome = document.getElementById('nome');
+const cpf = document.getElementById('cpf');
+const idade = document.getElementById('idade');
+const _id = document.getElementById('_id');
 
-const id = window.location.href.split("editar/")[1];
+const id = window.location.href.split('editar/')[1];
 
 getClienteById(id)
-    .then((x) => {
-        (nome.value = x.nome), (cpf.value = x.cpf), (idade.value = x.idade), (_id.value = x._id);
-    })
-    .catch((err) => console.log(err));
+	.then((x) => {
+		(nome.value = x.nome), (cpf.value = x.cpf), (idade.value = x.idade), (_id.value = x._id);
+	})
+	.catch((err) => console.log(err));
 
-formUpdate.addEventListener("submit", (e) => {
-    e.preventDefault();
+formUpdate.addEventListener('submit', (e) => {
+	e.preventDefault();
 
-    const data = {
-        _id: _id.value,
-        nome: nome.value,
-        idade: idade.value,
-        cpf: cpf.value,
-    };
+	const data = {
+		_id: _id.value,
+		nome: nome.value,
+		idade: idade.value,
+		cpf: cpf.value,
+	};
 
-    putCliente(data)
-        .then((x) => (window.location.href = "/clientes"))
-        .catch((err) => console.log(err));
+	putCliente(data)
+		.then((x) => {
+			setTimeout(() => {
+				window.location.href = '/clientes';
+			}, 2500);
+			toastr.error('Cliente atualizado');
+		})
+		.catch((err) => console.log(err));
 });
